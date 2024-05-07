eff28dcce705d31997bb218af3f916a92e671745
Sergey Passichenko <sergey@lekko.com>
2024-05-07 11:00:14 -0700

    fix type ignore

--- packages/transformer/src/ts-to-lekko.ts
+++ packages/transformer/src/ts-to-lekko.ts
@@ -153,19 +153,23 @@ function expressionToRule(
           },
         };
       } else if (tokenKind in LOGICAL_TOKEN_TO_OPERATOR) {
-        let rules : LekkoConfigJSONRule[] = [];
+        let rules: LekkoConfigJSONRule[] = [];
         const left = expressionToRule(checker, binaryExpr.left);
-        //@ts-ignore
-        if (left.logicalExpression !== undefined && left.logicalExpression.logicalOperator === LOGICAL_TOKEN_TO_OPERATOR[tokenKind]) {
-        //@ts-ignore
+        if (
+          "logicalExpression" in left &&
+          left.logicalExpression.logicalOperator ===
+            LOGICAL_TOKEN_TO_OPERATOR[tokenKind]
+        ) {
           rules = rules.concat(left.logicalExpression.rules);
         } else {
           rules.push(left);
         }
         const right = expressionToRule(checker, binaryExpr.right);
-        //@ts-ignore
-        if (right.logicalExpression !== undefined && right.logicalExpression.logicalOperator === LOGICAL_TOKEN_TO_OPERATOR[tokenKind]) {
-        //@ts-ignore
+        if (
+          "logicalExpression" in right &&
+          right.logicalExpression.logicalOperator ===
+            LOGICAL_TOKEN_TO_OPERATOR[tokenKind]
+        ) {
           rules = rules.concat(right.logicalExpression.rules);
         } else {
           rules.push(right);
