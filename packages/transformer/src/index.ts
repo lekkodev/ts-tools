27707d0a1c2ea8c3b1cadfdac47e7edfdb52709b
Sergey Passichenko <sergey@lekko.com>
2024-07-16 15:38:45 -0700

    add default export for transformer (#41)

--- packages/transformer/src/index.ts
+++ packages/transformer/src/index.ts
@@ -2,5 +2,6 @@ import { transformer } from "./transformer";
 import * as helpers from "./helpers";
 import { DotLekko, readDotLekko } from "./dotlekko";
 
+export default transformer;
 export { helpers, transformer, readDotLekko, DotLekko };
 export * as errors from "./errors";
